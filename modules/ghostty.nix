470aa5eebb0d3c0a54d22d40be3a67d6565cc340
TG-Jorj-2005 <tanase.george.1905@gmail.com>
2025-08-17 18:17:36 +0300

    Automated commit duminică 17 august 2025, 18:17:36 +0300

--- modules/ghostty.nix
+++ modules/ghostty.nix
@@ -1,39 +1,43 @@
-{config, pkgs, ...}:
+{ config, pkgs, ... }:
+
 {
-programs.ghostty ={
-  enable = true;
-  themes = {
-    catppuccin-mocha = {
-    background = "1e1e2e";
-    cursor-color = "f5e0dc";
-    foreground = "cdd6f4";
-    palette = [
-      "0=#45475a"
-      "1=#f38ba8"
-      "2=#a6e3a1"
-      "3=#f9e2af"
-      "4=#89b4fa"
-      "5=#f5c2e7"
-      "6=#94e2d5"
-      "7=#bac2de"
-      "8=#585b70"
-      "9=#f38ba8"
-      "10=#a6e3a1"
-      "11=#f9e2af"
-      "12=#89b4fa"
-      "13=#f5c2e7"
-      "14=#94e2d5"
-      "15=#a6adc8"
-    ];
-    selection-background = "353749";
-    selection-foreground = "cdd6f4";
-  };
+  programs.ghostty = {
+    enable = true;
+
+    # Tema Catppuccin minimală pentru start rapid
+    themes = {
+      catppuccin-mocha = {
+        background = "1e1e2e";
+        cursor-color = "f5e0dc";
+        foreground = "cdd6f4";
+        # Folosim palette minimală ca să nu încetinească
+        palette = [
+          "0=#45475a"
+          "1=#f38ba8"
+          "2=#a6e3a1"
+          "3=#f9e2af"
+          "4=#89b4fa"
+          "5=#f5c2e7"
+          "6=#94e2d5"
+          "7=#bac2de"
+        ];
+        selection-background = "353749";
+        selection-foreground = "cdd6f4";
+      };
     };
-   settings ={
+
+    # Setări rapide
+    settings = {
       font-size = 12;
-      background-opacity = 1;
+      background-opacity = 0.97;  # fără transparență la start
     };
 
-};
+    # Dezactivăm integrarea shell-ului pentru a accelera pornirea
+    enableBashIntegration = false;
+    enableZshIntegration = false;
 
+    # Tema implicită
+    defaultTheme = "catppuccin-mocha";
+  };
 }
+
