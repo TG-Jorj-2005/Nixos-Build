fbba88fb0c6ce49d9335e3e3010d45177b9aa0a2
TG-Jorj-2005 <tanase.george.1905@gmail.com>
2025-08-17 23:01:05 +0300

    Automated commit duminică 17 august 2025, 23:01:05 +0300

--- modules/alacritty.nix
+++ modules/alacritty.nix
@@ -151,26 +151,6 @@ in
         args = [ "--login" ];
       };
       
-      # Key bindings
-      keyboard.bindings = [
-        { key = "V"; mods = "Control|Shift"; action = "Paste"; }
-        { key = "C"; mods = "Control|Shift"; action = "Copy"; }
-        { key = "Insert"; mods = "Shift"; action = "PasteSelection"; }
-        { key = "Key0"; mods = "Control"; action = "ResetFontSize"; }
-        { key = "Equals"; mods = "Control"; action = "IncreaseFontSize"; }
-        { key = "Plus"; mods = "Control"; action = "IncreaseFontSize"; }
-        { key = "Minus"; mods = "Control"; action = "DecreaseFontSize"; }
-        { key = "F11"; mods = ""; action = "ToggleFullscreen"; }
-        { key = "Paste"; mods = ""; action = "Paste"; }
-        { key = "Copy"; mods = ""; action = "Copy"; }
-        { key = "L"; mods = "Control"; action = "ClearLogNotice"; }
-        { key = "L"; mods = "Control"; chars = "\\f"; }
-        { key = "PageUp"; mods = ""; action = "ScrollPageUp"; mode = "~Alt"; }
-        { key = "PageDown"; mods = ""; action = "ScrollPageDown"; mode = "~Alt"; }
-        { key = "Home"; mods = "Shift"; action = "ScrollToTop"; mode = "~Alt"; }
-        { key = "End"; mods = "Shift"; action = "ScrollToBottom"; mode = "~Alt"; }
-      ];
-      
       # Live config reload
       live_config_reload = true;
     };
