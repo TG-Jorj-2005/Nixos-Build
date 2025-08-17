699cf1c61ec3e15d6fa02fae9dbc085b826db2f0
TG-Jorj-2005 <tanase.george.1905@gmail.com>
2025-08-17 22:56:39 +0300

    Automated commit duminică 17 august 2025, 22:56:39 +0300

--- modules/alacritty.nix
+++ modules/alacritty.nix
@@ -2,76 +2,71 @@
 
 let
   catppuccinColors = {
-    background   = "#1e1e2e";
-    foreground   = "#cdd6f4";
-    cursor       = "#f5e0dc";
-    black        = "#45475a";
-    red          = "#f38ba8";
-    green        = "#a6e3a1";
-    yellow       = "#f9e2af";
-    blue         = "#89b4fa";
-    magenta      = "#f5c2e7";
-    cyan         = "#94e2d5";
-    white        = "#bac2de";
-    brightBlack  = "#585b70";
-    brightRed    = "#f38ba8";
-    brightGreen  = "#a6e3a1";
-    brightYellow = "#f9e2af";
-    brightBlue   = "#89b4fa";
-    brightMagenta= "#f5c2e7";
-    brightCyan   = "#94e2d5";
-    brightWhite  = "#a6adc8";
+    background    = "#1e1e2e";
+    foreground    = "#cdd6f4";
+    cursor        = "#f5e0dc";
+    black         = "#45475a";
+    red           = "#f38ba8";
+    green         = "#a6e3a1";
+    yellow        = "#f9e2af";
+    blue          = "#89b4fa";
+    magenta       = "#f5c2e7";
+    cyan          = "#94e2d5";
+    white         = "#bac2de";
+    brightBlack   = "#585b70";
+    brightRed     = "#f38ba8";
+    brightGreen   = "#a6e3a1";
+    brightYellow  = "#f9e2af";
+    brightBlue    = "#89b4fa";
+    brightMagenta = "#f5c2e7";
+    brightCyan    = "#94e2d5";
+    brightWhite   = "#a6adc8";
   };
 in
 {
-  programs.alacritty = {
-    enable = true;
+  programs.alacritty.enable = true;
 
-    # Font settings
-    font = {
-      normal = {
-        family = "FiraCode Nerd Font";
-        style = "Regular";
-      };
-      size = 12;
+  programs.alacritty.font = {
+    normal = {
+      family = "FiraCode Nerd Font";
+      style  = "Regular";
     };
-
-    # Colors (Catppuccin-Mocha)
-    colors = {
-      primary = {
-        background = catppuccinColors.background;
-        foreground = catppuccinColors.foreground;
-      };
-      cursor = {
-        text   = catppuccinColors.background;
-        cursor = catppuccinColors.cursor;
-      };
-      normal = {
-        black   = catppuccinColors.black;
-        red     = catppuccinColors.red;
-        green   = catppuccinColors.green;
-        yellow  = catppuccinColors.yellow;
-        blue    = catppuccinColors.blue;
-        magenta = catppuccinColors.magenta;
-        cyan    = catppuccinColors.cyan;
-        white   = catppuccinColors.white;
-      };
-      bright = {
-        black   = catppuccinColors.brightBlack;
-        red     = catppuccinColors.brightRed;
-        green   = catppuccinColors.brightGreen;
-        yellow  = catppuccinColors.brightYellow;
-        blue    = catppuccinColors.brightBlue;
-        magenta = catppuccinColors.brightMagenta;
-        cyan    = catppuccinColors.brightCyan;
-        white   = catppuccinColors.brightWhite;
-      };
-    };
-
-    # Other options
-    drawBoldTextWithBrightColors = true;
-    opacity = 0.96;
-    hideMouseWhenTyping = true;
+    size = 12;
   };
+
+  programs.alacritty.colors = {
+    primary = {
+      background = catppuccinColors.background;
+      foreground = catppuccinColors.foreground;
+    };
+    cursor = {
+      text   = catppuccinColors.background;
+      cursor = catppuccinColors.cursor;
+    };
+    normal = {
+      black   = catppuccinColors.black;
+      red     = catppuccinColors.red;
+      green   = catppuccinColors.green;
+      yellow  = catppuccinColors.yellow;
+      blue    = catppuccinColors.blue;
+      magenta = catppuccinColors.magenta;
+      cyan    = catppuccinColors.cyan;
+      white   = catppuccinColors.white;
+    };
+    bright = {
+      black   = catppuccinColors.brightBlack;
+      red     = catppuccinColors.brightRed;
+      green   = catppuccinColors.brightGreen;
+      yellow  = catppuccinColors.brightYellow;
+      blue    = catppuccinColors.brightBlue;
+      magenta = catppuccinColors.brightMagenta;
+      cyan    = catppuccinColors.brightCyan;
+      white   = catppuccinColors.brightWhite;
+    };
+  };
+
+  programs.alacritty.drawBoldTextWithBrightColors = true;
+  programs.alacritty.opacity                  = 0.96;
+  programs.alacritty.hideMouseWhenTyping     = true;
 }
 
