a4807538339593aafbe153275f362b8af56dc408
TG-Jorj-2005 <tanase.george.1905@gmail.com>
2025-08-17 15:55:12 +0300

    Automated commit duminică 17 august 2025, 15:55:12 +0300

--- assets/nvim.nix
+++ assets/nvim.nix
@@ -7,13 +7,11 @@ in
   programs.neovim = {
     enable = true;
     vimAlias = true;
-    package = pkgs.neovim;
 
     extraConfig = ''
-      " Adăugăm lazy.nvim în runtimepath
       set runtimepath+=${lazy_path}
 
-      " Configurăm LazyVim prin Lua
+      
       lua <<EOF
       local lazy = require("lazy")
       lazy.setup({
