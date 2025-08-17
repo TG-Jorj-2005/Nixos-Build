464cb4fb768b09acda6d4f744d42ec3353f1ba37
TG-Jorj-2005 <tanase.george.1905@gmail.com>
2025-08-17 22:58:46 +0300

    Automated commit duminică 17 august 2025, 22:58:46 +0300

--- modules/alacritty.nix
+++ modules/alacritty.nix
@@ -1,72 +1,178 @@
 { config, pkgs, ... }:
-
 let
   catppuccinColors = {
-    background    = "#1e1e2e";
-    foreground    = "#cdd6f4";
-    cursor        = "#f5e0dc";
-    black         = "#45475a";
-    red           = "#f38ba8";
-    green         = "#a6e3a1";
-    yellow        = "#f9e2af";
-    blue          = "#89b4fa";
-    magenta       = "#f5c2e7";
-    cyan          = "#94e2d5";
-    white         = "#bac2de";
-    brightBlack   = "#585b70";
-    brightRed     = "#f38ba8";
-    brightGreen   = "#a6e3a1";
-    brightYellow  = "#f9e2af";
-    brightBlue    = "#89b4fa";
-    brightMagenta = "#f5c2e7";
-    brightCyan    = "#94e2d5";
-    brightWhite   = "#a6adc8";
+    background    = "#1e1e2e";  # Mocha background
+    foreground    = "#cdd6f4";  # Mocha text
+    cursor        = "#f5e0dc";  # Mocha rosewater
+    
+    # Culori normale
+    black         = "#45475a";  # Mocha surface1
+    red           = "#f38ba8";  # Mocha pink
+    green         = "#a6e3a1";  # Mocha green
+    yellow        = "#f9e2af";  # Mocha yellow
+    blue          = "#89b4fa";  # Mocha blue
+    magenta       = "#f5c2e7";  # Mocha mauve
+    cyan          = "#94e2d5";  # Mocha teal
+    white         = "#bac2de";  # Mocha subtext1
+    
+    # Culori bright
+    brightBlack   = "#585b70";  # Mocha surface2
+    brightRed     = "#f38ba8";  # Mocha pink
+    brightGreen   = "#a6e3a1";  # Mocha green
+    brightYellow  = "#f9e2af";  # Mocha yellow
+    brightBlue    = "#89b4fa";  # Mocha blue
+    brightMagenta = "#f5c2e7";  # Mocha mauve
+    brightCyan    = "#94e2d5";  # Mocha teal
+    brightWhite   = "#a6adc8";  # Mocha subtext0
   };
 in
 {
-  programs.alacritty.enable = true;
-
-  programs.alacritty.font = {
-    normal = {
-      family = "FiraCode Nerd Font";
-      style  = "Regular";
+  programs.alacritty = {
+    enable = true;
+    
+    settings = {
+      # Configurare font
+      font = {
+        normal = {
+          family = "FiraCode Nerd Font";
+          style = "Regular";
+        };
+        bold = {
+          family = "FiraCode Nerd Font";
+          style = "Bold";
+        };
+        italic = {
+          family = "FiraCode Nerd Font";
+          style = "Italic";
+        };
+        size = 12.0;
+      };
+      
+      # Configurare culori Catppuccin Mocha
+      colors = {
+        primary = {
+          background = catppuccinColors.background;
+          foreground = catppuccinColors.foreground;
+          dim_foreground = "#7f849c";  # Mocha overlay1
+        };
+        
+        cursor = {
+          text = catppuccinColors.background;
+          cursor = catppuccinColors.cursor;
+        };
+        
+        vi_mode_cursor = {
+          text = catppuccinColors.background;
+          cursor = catppuccinColors.cursor;
+        };
+        
+        selection = {
+          text = catppuccinColors.background;
+          background = "#f5e0dc";  # Mocha rosewater
+        };
+        
+        normal = {
+          black   = catppuccinColors.black;
+          red     = catppuccinColors.red;
+          green   = catppuccinColors.green;
+          yellow  = catppuccinColors.yellow;
+          blue    = catppuccinColors.blue;
+          magenta = catppuccinColors.magenta;
+          cyan    = catppuccinColors.cyan;
+          white   = catppuccinColors.white;
+        };
+        
+        bright = {
+          black   = catppuccinColors.brightBlack;
+          red     = catppuccinColors.brightRed;
+          green   = catppuccinColors.brightGreen;
+          yellow  = catppuccinColors.brightYellow;
+          blue    = catppuccinColors.brightBlue;
+          magenta = catppuccinColors.brightMagenta;
+          cyan    = catppuccinColors.brightCyan;
+          white   = catppuccinColors.brightWhite;
+        };
+        
+        dim = {
+          black   = "#232634";
+          red     = "#e78284";
+          green   = "#a6d189";
+          yellow  = "#e5c890";
+          blue    = "#8caaee";
+          magenta = "#f4b8e4";
+          cyan    = "#81c8be";
+          white   = "#b5bfe2";
+        };
+      };
+      
+      # Setări generale
+      window = {
+        opacity = 0.96;
+        padding = {
+          x = 6;
+          y = 6;
+        };
+        dynamic_padding = true;
+        decorations = "full";
+        title = "Alacritty";
+        class = {
+          instance = "Alacritty";
+          general = "Alacritty";
+        };
+      };
+      
+      # Setări scrolling
+      scrolling = {
+        history = 10000;
+        multiplier = 3;
+      };
+      
+      # Setări mouse
+      mouse = {
+        hide_when_typing = true;
+        bindings = [
+          { mouse = "Middle"; action = "PasteSelection"; }
+        ];
+      };
+      
+      # Setări cursor
+      cursor = {
+        style = {
+          shape = "Block";
+          blinking = "Off";
+        };
+        unfocused_hollow = true;
+        thickness = 0.15;
+      };
+      
+      # Setări shell
+      shell = {
+        program = "${pkgs.zsh}/bin/zsh";
+        args = [ "--login" ];
+      };
+      
+      # Key bindings
+      keyboard.bindings = [
+        { key = "V"; mods = "Control|Shift"; action = "Paste"; }
+        { key = "C"; mods = "Control|Shift"; action = "Copy"; }
+        { key = "Insert"; mods = "Shift"; action = "PasteSelection"; }
+        { key = "Key0"; mods = "Control"; action = "ResetFontSize"; }
+        { key = "Equals"; mods = "Control"; action = "IncreaseFontSize"; }
+        { key = "Plus"; mods = "Control"; action = "IncreaseFontSize"; }
+        { key = "Minus"; mods = "Control"; action = "DecreaseFontSize"; }
+        { key = "F11"; mods = ""; action = "ToggleFullscreen"; }
+        { key = "Paste"; mods = ""; action = "Paste"; }
+        { key = "Copy"; mods = ""; action = "Copy"; }
+        { key = "L"; mods = "Control"; action = "ClearLogNotice"; }
+        { key = "L"; mods = "Control"; chars = "\\f"; }
+        { key = "PageUp"; mods = ""; action = "ScrollPageUp"; mode = "~Alt"; }
+        { key = "PageDown"; mods = ""; action = "ScrollPageDown"; mode = "~Alt"; }
+        { key = "Home"; mods = "Shift"; action = "ScrollToTop"; mode = "~Alt"; }
+        { key = "End"; mods = "Shift"; action = "ScrollToBottom"; mode = "~Alt"; }
+      ];
+      
+      # Live config reload
+      live_config_reload = true;
     };
-    size = 12;
   };
-
-  programs.alacritty.colors = {
-    primary = {
-      background = catppuccinColors.background;
-      foreground = catppuccinColors.foreground;
-    };
-    cursor = {
-      text   = catppuccinColors.background;
-      cursor = catppuccinColors.cursor;
-    };
-    normal = {
-      black   = catppuccinColors.black;
-      red     = catppuccinColors.red;
-      green   = catppuccinColors.green;
-      yellow  = catppuccinColors.yellow;
-      blue    = catppuccinColors.blue;
-      magenta = catppuccinColors.magenta;
-      cyan    = catppuccinColors.cyan;
-      white   = catppuccinColors.white;
-    };
-    bright = {
-      black   = catppuccinColors.brightBlack;
-      red     = catppuccinColors.brightRed;
-      green   = catppuccinColors.brightGreen;
-      yellow  = catppuccinColors.brightYellow;
-      blue    = catppuccinColors.brightBlue;
-      magenta = catppuccinColors.brightMagenta;
-      cyan    = catppuccinColors.brightCyan;
-      white   = catppuccinColors.brightWhite;
-    };
-  };
-
-  programs.alacritty.drawBoldTextWithBrightColors = true;
-  programs.alacritty.opacity                  = 0.96;
-  programs.alacritty.hideMouseWhenTyping     = true;
 }
-
