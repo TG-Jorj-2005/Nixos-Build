043890310217823c07ac1a1fd42078643d163a46
TG-Jorj-2005 <tanase.george.1905@gmail.com>
2025-08-17 15:29:33 +0300

    Automated commit duminică 17 august 2025, 15:29:33 +0300

--- assets/nvim.nix
+++ assets/nvim.nix
@@ -5,7 +5,22 @@ programs.neovim = {
     vimAlias = true;
     plugins = with pkgs.vimPlugins;[
     LazyVim  ];
-};
+    extraConfig = ''
+      " Clonăm LazyVim într-un folder local
+      if empty(glob(expand("~/.config/nvim/lazy/lazy.nvim")))
+        silent !git clone --filter=blob:none https://github.com/folke/lazy.nvim.git --branch=stable ~/.config/nvim/lazy/lazy.nvim
+      endif
+
+      " Setăm runtimepath
+      set runtimepath+=~/.config/nvim/lazy/lazy.nvim
+
+      " LazyVim init.lua
+      lua << EOF
+      require("lazy").setup("LazyVim/LazyVim", {})
+      EOF
+    '';
+
+    };
 
 
 }
