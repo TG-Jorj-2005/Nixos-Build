326ee557427f9656e794171002f0ce6806a03706
TG-Jorj-2005 <tanase.george.1905@gmail.com>
2025-08-17 17:03:45 +0300

    Automated commit duminică 17 august 2025, 17:03:45 +0300

--- modules/nvf.nix
+++ modules/nvf.nix
@@ -3,8 +3,6 @@
 
 programs.nvf = {
     enable = true;
-    # your settings need to go into the settings attribute set
-    # most settings are documented in the appendix
     settings = {
       vim.viAlias = false;
       vim.vimAlias = true;
@@ -12,4 +10,5 @@ programs.nvf = {
         enable = true;
       };
     };
-  };}
+  };
+  }
