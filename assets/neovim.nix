af0920678e1b15d6c7c7974ac4064188af822657
TG-Jorj-2005 <tanase.george.1905@gmail.com>
2025-08-19 15:23:11 +0300

    Automated commit marți 19 august 2025, 15:23:11 +0300

--- assets/neovim.nix
+++ assets/neovim.nix
@@ -1,23 +1,28 @@
 {config, pkgs, ...}:
 {
-programs.neovim = {
-  # ...
-  plugins = [ pkgs.vimPlugins.lazy-nvim ];
-  extraLuaConfig =
-    # lua
-    ''
-      require("lazy").setup({
-        -- disable all update / install features
-        -- this is handled by nix
-        rocks = { enabled = false },
-        pkg = { enabled = false },
-        install = { missing = false },
-        change_detection = { enabled = false },
-        spec = {
-          -- TODO
+rograms.neovim = {
+  plugins = with pkgs.vimPlugins; [
+    lazy-nvim
+  ];
+
+  extraLuaConfig = ''
+    vim.g.mapleader = " " -- Need to set leader before lazy for correct keybindings
+    require("lazy").setup({
+      performance = {
+        reset_packpath = false,
+        rtp = {
+            reset = false,
+          }
         },
-      })
-    '';
+      dev = {
+        path = "${pkgs.vimUtils.packDir config.home-manager.users.USERNAME.programs.neovim.finalPackage.passthru.packpathDirs}/pack/myNeovimPackages/start",
+      },
+      install = {
+        -- Safeguard in case we forget to install a plugin with Nix
+        missing = false,
+      },
+    })
+  '';
 };
 
 
