e99b6811cccb75b4356cca8403d9b45f783ced8f
TG-Jorj-2005 <tanase.george.1905@gmail.com>
2025-08-22 16:46:44 +0300

    Automated commit vineri 22 august 2025, 16:46:44 +0300

--- configuration.nix
+++ configuration.nix
@@ -167,5 +167,34 @@
         };
 	settings.auto-optimise-store = true;
       };
+   #Neovim LazyVim
+
+   programs.neovim = {
+  enable = true;
+  defaultEditor = true;
+  extraLuaConfig = ''
+    -- Bootstrap lazy.nvim
+    local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+    if not vim.loop.fs_stat(lazypath) then
+      vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable",
+        lazypath,
+      })
+    end
+    vim.opt.rtp:prepend(lazypath)
+
+    require("lazy").setup({
+      spec = {
+        { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+        { import = "lazyvim.plugins.extras.lang.python" },
+        { import = "lazyvim.plugins.extras.lang.typescript" },
+      },
+    })
+  '';
+};
 
 }
