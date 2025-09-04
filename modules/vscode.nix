5d580edc31916a4c80570545cbf8c7d4b760ed8e
TG-Jorj-2005 <tanase.george.1905@gmail.com>
2025-09-04 21:47:35 +0300

    Automated commit joi 4 septembrie 2025, 21:47:35 +0300

--- modules/vscode.nix
+++ modules/vscode.nix
@@ -43,11 +43,6 @@
     pkgs.raylib
     pkgs.libx11
   ];
-   home.sessionVariables = {
-    LIBRARY_PATH = "${pkgs.xorg.libX11}/lib:${pkgs.libGL}/lib:$LIBRARY_PATH";
-    LD_LIBRARY_PATH = "${pkgs.xorg.libX11}/lib:${pkgs.libGL}/lib:$LD_LIBRARY_PATH";
-    CPATH = "${pkgs.xorg.libX11}/include:${pkgs.libGL}/include:$CPATH";
-  };
 
 }
 
