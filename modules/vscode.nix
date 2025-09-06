40b31dcdb6de847078a3c24945a4a375381d4c25
TG-Jorj-2005 <tanase.george.1905@gmail.com>
2025-09-06 19:22:16 +0300

    Automated commit sâmbătă 6 septembrie 2025, 19:22:16 +0300

--- modules/vscode.nix
+++ modules/vscode.nix
@@ -8,29 +8,27 @@
     nixpkgs-fmt nixd
   ];
 
-  programs.vscode = {
-    enable = true;
-    package = pkgs.vscode;
-    profiles.default = {
-      extensions = with pkgs.vscode-extensions; [
-        ms-vscode.cpptools
-        ms-vscode.cmake-tools
-        twxs.cmake
-        jnoortheen.nix-ide
-        jeff-hykin.better-nix-syntax
-      ];
-      userSettings = {
-        "editor.formatOnSave" = true;
-        "files.autoSave" = "afterDelay";
-        "C_Cpp.default.cppStandard" = "c++20";
-        "C_Cpp.default.cStandard" = "c17";
-        "nix.enableLanguageServer" = true;
-        "nix.serverPath" = "nixd";
-        "nix.formatterPath" = "nixpkgs-fmt";
-      };
+ programs.vscode = {
+  enable = true;
+  package = pkgs.vscode;
+  profiles.default = {
+    extensions = with pkgs.vscode-extensions; [
+      ms-vscode.cpptools
+      ms-vscode.cmake-tools
+      twxs.cmake
+      jnoortheen.nix-ide
+      jeff-hykin.better-nix-syntax
+    ];
+    userSettings = {
+      "editor.formatOnSave" = true;
+      "files.autoSave" = "afterDelay";
+      "C_Cpp.default.cppStandard" = "c++20";
+      "C_Cpp.default.cStandard" = "c17";
+      "nix.enableLanguageServer" = true;
+      "nix.serverPath" = "nixd";
+      "nix.formatterPath" = "nixpkgs-fmt";
     };
   };
-
-  home-manager.backupFileExtension = "backup";
+};
 }
 
